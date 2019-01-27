9becaa6c19560ba1f10e915ce364b5e7386f7c14
Golam Gause Jaman <jamagola@isu.edu>
2019-01-27 03:45:13 -0700

    Add files via upload

---
+++ curie.ino
@@ -0,0 +1,49 @@
+/********************************************
+ * This program is intended for reading
+ * ADC values and transmit over UART 
+ * across multiple platform.
+ * Author: Golam Gause Jaman
+ * email: jamagola@isu.edu
+ ********************************************/
+
+#include <math.h>
+
+char *buff;
+int L=4;
+int sensor;
+int dt=100; //0.1 sec.
+int pin=A0;
+
+void numerASCII(char*, int, int);
+void numerASCII(char*, int);
+
+void setup() {
+  buff = new char[L];
+  Serial.begin(9600); //Initializing UART with baud rate
+}
+
+void loop() {
+  sensor=analogRead(pin); //Reading from ADC channel
+  //Convert and transmit over UART
+  numerASCII(buff, sensor, L);
+  //numerASCII(buff, sensor);
+  for(int i=0; i<L; i++) Serial.print(buff[i]);
+  //Serial.print(buff);
+  delay(dt);
+}
+
+// Convert numerical reading to ASCII character
+void numerASCII(char *buff, int sensor, int L)
+{
+  for(int i=L-1; i>=0; i--)
+  {
+    buff[i]=sensor%10 + '0';
+    sensor/=10;
+    }
+}
+
+void numerASCII(char *buff, int sensor)
+{ 
+  sprintf(buff,"%d\n",sensor);
+}
+
---
+++ uartOctave.m
@@ -0,0 +1,49 @@
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ % This program is intended for reading ADC
+ % values from external source over UART. 
+ % Author: Golam Gause Jaman
+ % email: jamagola@isu.edu
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ 
+ clear all;
+ clc;
+ 
+ pkg load instrument-control
+ obj = serial('COM3');
+ set(obj, 'baudrate',9600);
+ set(obj, 'timeout',20); % 2 seconds time out
+ 
+ limit=60000;
+ dt=0.1; %Interval
+ L=4; %Length
+ ul=1023; %Max reading
+ 
+ ex=zeros(1,L);
+ for e=0:L-1
+   ex(L-e)=10^e;
+ end
+ 
+ % Display info.
+ obj
+ fopen(obj);
+ ws = zeros(1,limit+1);
+ % UART(starts receiving data)
+ for i=2:limit
+   out=srl_read(obj,4);
+   ws(i)=(double(out)-48)*ex';
+   
+   % Plot
+   plot([i-1,i],[ws(i-1),ws(i)],'r-');
+   grid on;
+   hold on;
+   if i<=51  %x window
+    axis([1,51,0,ul]);
+  else
+    axis([i-50,i,0,ul]);
+  end
+  xlabel('Sample');
+  ylabel('Amplitude');
+  pause(dt*0.5);
+ end
+ 
+ fclose(obj);
